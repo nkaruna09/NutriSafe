ee08695e15b7e228374fdcc0f887c2e5638889c9
Nithika <nithika.karuna@gmail.com>
2025-01-11 14:18:09 -0500

    create title

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+#import requests
+#import openai
+#from utils.barcode_lookup import fetch_product_data, is_food_item
+#from utils.drug_list import fetch_drug_list
+#from utils.analysis import analyze_interaction
+
+st.title("NutriSafe")
