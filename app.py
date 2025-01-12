8c345bb342217c2b270e1a25fcb40bb26654743d
Zuhair Qureshi <quresz23@mcmaster.ca>
2025-01-11 23:55:44 -0500

    previous version

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import plotly.express as px
+
+#import requests
+#import openai
+#from utils.barcode_lookup import fetch_product_data, is_food_item
+#from utils.drug_list import fetch_drug_list
+#from utils.analysis import analyze_interaction
 
 st.set_page_config(page_title="NutriSafe", layout="centered")
 
@@ -10,13 +17,13 @@ with open("styles.css") as f:
 
 col1, col2 = st.columns([0.15, 0.85])
 
-with col1: 
+with col1:
     st.image("assets/nutrisafe_logo.png")
 with col2:
     st.title("Welcome to NutriSafe!")
 
 # Configure sidebar for multiple pages 
-pages = [st.Page("how_it_works.py", title="Learn About NutriSafe"), st.Page("check_food_safety.py", title="Try NutriSafe Out"), st.Page("user_history.py", title="Barcode History")]
+pages = [st.Page("how_it_works.py", title="Learn About NutriSafe"), st.Page("check_food_safety.py", title="Try NutriSafe Out")]
 pg = st.navigation(pages)
 pg.run()
 
