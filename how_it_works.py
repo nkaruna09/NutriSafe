e28ce5c0b7fa8178fd69f43073d4101fb4f8ea77
Emily Su <emilysucanada@gmail.com>
2025-01-12 03:20:28 -0500

    edit demo videos

--- how_it_works.py
+++ how_it_works.py
@@ -8,21 +8,21 @@ st.write("1. **Select your ailment/disease** and any **allergens** from the opti
 # First video demonstration 
 ailment_video = open("assets/select_ailment.mp4", "rb")
 ailment_vid_bytes = ailment_video.read()
-st.video(ailment_vid_bytes)
+st.video(ailment_vid_bytes, loop=True, autoplay=True, muted=True)
 
 st.write("2. **Enter or upload a picture of the barcode** of the food product you want to check.")
 
 # Second video demonstration 
 barcode_video = open("assets/barcode.mp4", "rb")
 barcode_vid_bytes = barcode_video.read()
-st.video(barcode_vid_bytes)
+st.video(barcode_vid_bytes, loop=True, autoplay=True, muted=True)
 
 st.write("3. **Get the safety status** along with recommendations or alternatives.")
 
 # Third video demonstration 
 status_video = open("assets/safety_status.mp4", "rb")
 status_vid_bytes = status_video.read()
-st.video(status_vid_bytes)
+st.video(status_vid_bytes, loop=True, autoplay=True, muted=True)
 
 st.write("4. **Explore additional NutriSafe features,** such as talking to an AI health coach.")
 
